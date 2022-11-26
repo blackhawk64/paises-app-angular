426e0bf5d54259e6640547c1de52ad233b1df877
Alexis Alcantara <alexisv.alcantara@gmail.com>
2022-11-25 18:54:09 -0600

    Se agrego funcion de debounce para mostrar sugerencias en busqueda de paises por capital

--- src/app/paises/pages/por-capital/por-capital.component.ts
+++ src/app/paises/pages/por-capital/por-capital.component.ts
@@ -5,18 +5,27 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
-  styles: [],
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class PorCapitalComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Pais[] = [];
+  paisesSugeridos: Pais[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private apiService: PaisService) {}
 
   buscar(termino: string) {
     this.termino = termino.trim();
     this.hayError = false;
+    this.mostrarSugerencias = false;
 
     if (this.termino.length > 0) {
       this.apiService.buscarPorCapital(this.termino).subscribe({
@@ -29,5 +38,20 @@ export class PorCapitalComponent {
         },
       });
     }
+  }
+
+  sugerencias(termino: string) {
+    this.hayError = false;
+    this.mostrarSugerencias = true;
+    this.termino = termino;
+
+    this.apiService.buscarPorCapital(termino).subscribe({
+      next: (response) => {
+        this.paisesSugeridos = response.splice(0, 5);
+      },
+      error: () => {
+        this.paisesSugeridos = [];
+      },
+    });
   }
 }
